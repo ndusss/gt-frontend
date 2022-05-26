d1dffac2f411cde765690f8f9ae26c2e0ae72306
ndusss <rosandus@protonmail.com>
2022-05-26 23:44:24 +0700

    Completed MVP

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+// module.exports = {
+//   purge: {
+//     enabled: true,
+//     content: ["src/*.html"],
+//   },
+//   darkMode: false, // or 'media' or 'class'
+//   theme: {
+//     extend: {
+//       colors: {
+//         'ghost-task': "#F89283"
+//       }
+//     },
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
